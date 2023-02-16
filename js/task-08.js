f8658b6cfbc1e7000f8d17e4b1a1de0dbfa22328
Valerii Pometun <vmpometun@gmail.com>
2023-02-16 09:53:46 +0200

    some training - #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) { 
+    event.preventDefault();
+
+    formRef.elements.email.required = true
+    formRef.elements.password.required = true
+
+    if (formRef.elements.email.required || formRef.elements.password.required) {
+        alert('stop')
+    }
+
+
+};
